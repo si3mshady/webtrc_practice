4afaa258975b60c472fdbd1a18776f8ad5db7afe
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2024-01-11 03:54:15 -0600

    save point

---
+++ public/client.js
@@ -0,0 +1,40 @@
+const socket = io()
+const peer = new RTCPeerConnection();
+
+const shareButtom = document.getElementById("screen-share")
+
+shareButtom.addEventListener("click", async () => {
+    const config = {
+        audio: true,
+        video: true,
+        preferCurrentTab: true
+    }
+
+    try {
+      const videoStream = await navigator.mediaDevices.getDisplayMedia(config)    
+      peer.addTrack(videoStream.getVideoTracks()[0] , videoStream)
+
+      const sdp = await peer.createOffer()
+      await peer.setLocalDescription(sdp)
+
+      socket.emit('sdp-offer', peer.localDescription)
+
+    } catch (error) {
+        console.log(error)
+        alert(error.message)
+    }
+})
+
+socket.on('sdp-answer', async (remoteSDP) => {
+    peer.setRemoteDescription(remoteSDP)
+} );
+
+peer.addEventListener('icecandidate', (event) => {
+    if (event.candidate) {
+        socket.emit('ice-candidate', event.candidate)
+    }
+} );
+
+socket.on('ice-candidate', async (candidate) => {
+    await peer.addIceCandidate(new RTCIceCandidate(candidate))
+})
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const {Server: SocketServer } = require( "socket.io")
+const port = 3000
+
+const app = express()
+
+
+app.use('/', express.static('public'));
+
+const httpServer = app.listen(port, () => {
+    console.log('Server started on port ', port)
+})
+
+const io = new SocketServer(httpServer);
+
+io.on("connection", (socket) => {
+    console.log('new connection from ', socket.id)
+
+    socket.on("sdp-offer", (offer) => { 
+        console.log('New SDP offer from local user ', socket.id);
+        console.log("SPD OFFER ", offer)
+        socket.broadcast.emit('sdp-offer', offer)
+    });
+
+
+    socket.on("sdp-answer", (answer) => { 
+        console.log('New SDP answer from remote user ', socket.id);
+        console.log("SDP ANSWER from remote user ", answer)        
+        socket.broadcast.emit('sdp-answer', offer)
+    });
+
+    socket.on("ice-candidate", (candidate) => { 
+        console.log('New ICE candidate from ', socket.id);
+        console.log("ICE candidate ", candidate)        
+        socket.broadcast.emit('ice-candidate', candidate)
+    });
+
+    
+
+
+
+
+    socket.on
+
+})
